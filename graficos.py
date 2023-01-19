156e631e197a8df2167f29cddbef48743679e30e
leonardoMD2 <100425097+leonardoMD2@users.noreply.github.com>
2023-01-19 00:11:06 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -13,14 +13,6 @@ def graficaPastel(labels, values):
   eje.axis('equal')
   plt.show()
 
-
-def generate_pie_chart(labels, values):
-  fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
-  ax.axis('equal')
-  plt.show()
-
-
 if __name__ == '__main__':
   
   indices = ['a','b','c']
